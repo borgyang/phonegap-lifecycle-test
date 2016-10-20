48b97c827c1df96d14d63d922be8c07ec63b967f
borgyang <yanb1_15@uni.worc.ac.uk>
2016-10-20 14:02:36 +0100

    store

--- www/js/index.js
+++ www/js/index.js
@@ -1,16 +1,35 @@
 var paused_count =0;
 var resumed_count = 0;
 var launched_count = 0;
+var key="pen";
+var value="blue";
+var key="book";
+var value="black";
+var key="cat";
+var value="yellow";
+var key="dog";
+var value="red";
+var key="mouse";
+var value="black";
+
+var key = prompt("Please enter your key");
+if (key != null) {
+    document.getElementById("demo").innerHTML =
+    "you colour " + value ;
+}
+window.localStorage.setItem( key, value );
 
     function onLoad() {
         document.addEventListener("deviceready", onDeviceReady, false);
 		console.log("device ready");
+        
     }
 	
 	function updateDisplay() {
 		$("#launched").text("Application launched: " + launched_count);
 		$("#resumed").text("Application paused: " + paused_count);
 		$("#paused").text("Application resumed: " + resumed_count);
+        alert("");
 	}
 
 
